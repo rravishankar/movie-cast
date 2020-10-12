5c77483a5e041d5c2d4b88e8ca0c3e882c721899
rravishankar <rravi108@gmail.com>
2020-10-12 21:00:56 +0530

    Updating deployment mode to production

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export MOVIE_DB_PASSWORD="postgres" #DEPLOY_MODE=dev that is local database
 export AUTH0_DOMAIN_NAME="cralina-test.us.auth0.com" #From Auth0
 export AUTH0_CLIENT_ID="N407SsqHjEtAoZIeXc4BNkAttaKiNyTy" #From Auth0
 export API_IDENTIFIER="movie" #From Auth0
-export DEPLOY_MODE="dev" #"dev" is for development mode, else it's production. Used for database
+export DEPLOY_MODE="prod" #"dev" is for development mode, else it's production. Used for database
 export LOG_LEVEL="DEBUG" #Valid value is DEBUG else the logger logs INFO & above #TBD to be fixed to include all levels. Used in app.py
 #TBD Repetitive need to be fixed in future release - check above variable names
 export AUTH0_DOMAIN='cralina-test.us.auth0.com' #From Auth0
